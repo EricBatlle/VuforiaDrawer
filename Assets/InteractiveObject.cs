cc17e4584b696fca95dce9be68f5be0a5de5e65d
Eric Batlle Clavero <erikbc_95@hotmail.com>
2018-05-28 13:12:09 +0200

    added interactiveObject for future networking

---
+++ Assets/InteractiveObject.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Networking;
+
+public class InteractiveObject : NetworkBehaviour, IPointerClickHandler {
+
+    [SerializeField] private GameObject cube;
+    [SerializeField] private Material[] materials;
+    private int materialsCount = 0;
+
+    public Material currMaterial;
+    private Material startMaterial;
+
+    private void Start()
+    {
+        cube = GameObject.FindWithTag("Cube");
+        currMaterial = cube.GetComponent<Renderer>().material;
+        startMaterial = currMaterial;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+
+        print("ONCLICK");
+        nextColor();
+    }
+
+    #region nextColor
+    private void nextColor()
+    {
+        if (isServer)
+        {
+            print("ISSERVER");
+
+            RpcNextColor();
+        }
+        else
+        {
+            print("ISCLIENT");
+
+            CmdNextColor();
+        }
+    }
+
+    [Command]
+    private void CmdNextColor()
+    {
+        RpcNextColor();
+    }
+    [ClientRpc]
+    private void RpcNextColor()
+    {
+        if (materialsCount >= materials.Length)
+        {
+            materialsCount = 0;
+        }
+        cube.GetComponent<Renderer>().material = materials[materialsCount];
+        currMaterial = materials[materialsCount];
+        materialsCount++;
+    }
+    #endregion
+}
